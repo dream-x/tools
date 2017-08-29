c60250bfe6b304c639478e29942cb19a57b71ae7
Max Kozlov <dreamx.max@gmail.com>
2017-08-29 19:22:37 +0300

    Config file parametr and add README.

---
+++ fork_updater/README.md
@@ -0,0 +1,7 @@
+# Fork updater
+
+1. Clone this repo.
+2. Edit `.config` and move for user path. 
+3. Execute `$ chmod +x fork_up.sh`.
+4. Create symlink `$ ln -s <full_path>/fork_up.sh\ <full_path>/.config /usr/local/bin/fup`
+5. Enjoy `$ fup`!
--- fork_updater/fork_up.sh
+++ fork_updater/fork_up.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-config_path="/Users/kmax/github/_dreamx/tools/fork_updater/.config"
+config_path=$1
 
 while read line; do    
   path=`echo $line | cut -d, -f1`
@@ -9,4 +9,3 @@ while read line; do
   
   cd $path; git checkout $branch; git fetch $remote; git merge $remote/$branch; git push origin $branch
 done < $config_path
-
