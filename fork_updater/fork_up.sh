c3e5d90af6eee8fb92466da69248e153f57820f5
Max Kozlov <dreamx.max@gmail.com>
2017-08-29 18:28:53 +0300

    v0.1

--- fork_updater/fork_up.sh
+++ fork_updater/fork_up.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
+config_path="/Users/kmax/github/_dreamx/tools/fork_updater/.config"
+
+while read line; do    
+  path=`echo $line | cut -d, -f1`
+  remote=`echo $line | cut -d, -f2`
+  branch=`echo $line | cut -d, -f3`
+  
+  cd $path; git checkout $branch; git fetch $remote; git merge $remote/$branch; git push origin $branch
+done < $config_path
 
