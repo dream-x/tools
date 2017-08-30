73374b712bf07094b7296f6992ba5a689f7a6b5f
Max Kozlov <dreamx.max@gmail.com>
2017-08-30 11:13:35 +0300

    Return current state repo.

--- fork_updater/fork_up.sh
+++ fork_updater/fork_up.sh
@@ -3,9 +3,15 @@
 config_path=$1
 
 while read line; do    
-  path=`echo $line | cut -d, -f1`
-  remote=`echo $line | cut -d, -f2`
-  branch=`echo $line | cut -d, -f3`
+  path=`echo $line | cut -d \, -f 1`
+  remote=`echo $line | cut -d \, -f 2`
+  branch=`echo $line | cut -d \, -f 3`
   
-  cd $path; git checkout $branch; git fetch $remote; git merge $remote/$branch; git push origin $branch
+  cd $path;
+
+  current_branch=`git branch | grep "*" | cut -d \* -f 2 | awk '{print $1}'`
+
+  cd $path; git stash; git checkout $branch; git fetch $remote; 
+  git merge $remote/$branch; git push origin $branch; 
+  git checkout $current_branch; git stash pop;
 done < $config_path
